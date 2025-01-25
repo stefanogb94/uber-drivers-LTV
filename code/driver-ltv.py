25e4d1927e0200ec4f0e9ff654070cbdbf8701a3
stefanogb94 <stefano.gb94@gmail.com>
2025-01-25 01:31:57 +0100

    calculating LTV and adjusted LTV based on working intensity

---
+++ code/driver-ltv.py
@@ -0,0 +1,14 @@
+
+# %%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+drivers = pd.read_csv('./data/driver_ids.csv')
+drivers.head()
+
+rides = pd.read_csv('./data/ride_ids.csv')
+
+ride_timestamp1 = pd.read_csv('./data/ride_timestamp_part_1.csv')
+# %%
